c02502783d6fedb125630cf110111176bda82159
elenintoulou <elenintoulou@hotmail.com>
2025-10-24 17:23:09 +0300

    Add: UserEditDTO

---
+++ src/main/java/gr/shareabite/app/dto/UserEditDTO.java
@@ -0,0 +1,43 @@
+package gr.shareabite.app.dto;
+
+import gr.shareabite.app.model.static_data.Region;
+import gr.shareabite.app.model.static_data.Role;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserEditDTO {
+
+    @Size(min = 2, max = 30, message = "The username must be between 2 - 30 characters.")
+    private String username;
+
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$",
+            message = "The password must have at least 1 small character, 1 capital character, " +
+                    "1 number and 1 symbol without spaces. ")
+    private String password;
+
+    @NotNull(message = "The role cannot be null.")
+    private Role role;
+
+    @NotNull(message = "The region cannot be null.")
+    private Region region;
+
+    @NotNull(message = "The email cannot be null.")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "Invalid email address.")
+    private String email;
+
+    @Pattern(
+            regexp = "^(\\+\\d{1,3}[- ]?)?\\d{7,15}$",
+            message = "Invalid phone number.")
+    private String phoneNumber;
+
+}
