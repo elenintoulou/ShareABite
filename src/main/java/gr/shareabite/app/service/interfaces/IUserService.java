5806aea6d9a22f25cef571aa949065f8ea2f4957
elenintoulou <elenintoulou@hotmail.com>
2025-10-26 20:11:22 +0200

    Add more methods

--- src/main/java/gr/shareabite/app/service/interfaces/IUserService.java
+++ src/main/java/gr/shareabite/app/service/interfaces/IUserService.java
@@ -1,9 +1,14 @@
 package gr.shareabite.app.service.interfaces;
 
+import gr.shareabite.app.dto.UserEditDTO;
 import gr.shareabite.app.dto.UserInsertDTO;
+import gr.shareabite.app.dto.UserRegisterDTO;
 import gr.shareabite.app.exceptions.EntityAlreadyExistsException;
+import gr.shareabite.app.exceptions.NotExistingEntityException;
 
 public interface IUserService {
 
     void saveUser(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException;
+    void registerUser(UserRegisterDTO userRegisterDTO) throws EntityAlreadyExistsException;
+    void editUser(UserEditDTO userEditDTO) throws NotExistingEntityException;
 }
