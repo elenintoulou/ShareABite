d78a24ee2f47ef35d4d5a88b53d9b465839b396c
elenintoulou <elenintoulou@hotmail.com>
2025-10-26 18:26:49 +0200

    Add: UserSignUpDTO

---
+++ src/main/java/gr/shareabite/app/dto/UserSignUpDTO.java
@@ -0,0 +1,34 @@
+package gr.shareabite.app.dto;
+
+import gr.shareabite.app.model.static_data.Region;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserSignUpDTO {
+
+    @NotNull(message = "The username cannot not be null.")
+    @Size(min = 2, max = 30, message = "The username must be between 2 - 30 characters.")
+    private String username;
+
+    @NotNull(message = "The password cannot not be null.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$",
+            message = "The password must have at least 1 small character, 1 capital character, " +
+                    "1 number and 1 symbol without spaces. ")
+    private String password;
+
+    @NotNull(message = "The email cannot be null.")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "Invalid email address.")
+    private String email;
+
+    private Region region;
+}
