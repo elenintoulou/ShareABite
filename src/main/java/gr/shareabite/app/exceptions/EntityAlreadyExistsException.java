5979e1d1fb3ce3703c55b14cd46ea501ebe7fe89
elenintoulou <elenintoulou@hotmail.com>
2025-10-26 20:10:42 +0200

    Fix: EntityAlreadyExistsException class

--- src/main/java/gr/shareabite/app/exceptions/EntityAlreadyExistsException.java
+++ src/main/java/gr/shareabite/app/exceptions/EntityAlreadyExistsException.java
@@ -1,4 +1,10 @@
 package gr.shareabite.app.exceptions;
 
-public class EntityAlreadyExistsException extends Exception{
+
+public class EntityAlreadyExistsException extends Exception {
+    private static final String DEFAULT_CODE = "Already exists";
+
+    public EntityAlreadyExistsException(String code, String message) {
+        super(code + DEFAULT_CODE + message);
+    }
 }
