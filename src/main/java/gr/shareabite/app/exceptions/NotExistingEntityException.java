a34b899581a364242c86c919457d0f7b0625250a
elenintoulou <elenintoulou@hotmail.com>
2025-10-27 19:38:39 +0200

    Add some code in the exception class

--- src/main/java/gr/shareabite/app/exceptions/NotExistingEntityException.java
+++ src/main/java/gr/shareabite/app/exceptions/NotExistingEntityException.java
@@ -1,6 +1,10 @@
 package gr.shareabite.app.exceptions;
 
 public class NotExistingEntityException extends Exception {
+    private static final String DEFAULT_CODE = "Does not exist";
 
+    public NotExistingEntityException(String code, String message) {
+        super(code + DEFAULT_CODE + message);
+    }
 
 }
