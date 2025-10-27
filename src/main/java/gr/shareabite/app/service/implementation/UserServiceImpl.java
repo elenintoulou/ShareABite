7341ba7c1d73fe06b0d789c57712e5a820b452f3
elenintoulou <elenintoulou@hotmail.com>
2025-10-27 19:39:51 +0200

    Update: override the methods

--- src/main/java/gr/shareabite/app/service/implementation/UserServiceImpl.java
+++ src/main/java/gr/shareabite/app/service/implementation/UserServiceImpl.java
@@ -12,17 +12,18 @@ import gr.shareabite.app.service.interfaces.IUserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.mapper.Mapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+
+//FIX MY SERVICEEEEEEEEEEEE!!!!!!!!
 public class UserServiceImpl implements IUserService {
 
     //Dependency injection
     private final UserRepository userRepository;
-    private final Mapper mapper;
+  //add a mapper
     //i should add a class for authentication
     private final RoleRepository roleRepository;
 
@@ -31,31 +32,63 @@ public class UserServiceImpl implements IUserService {
     @Override
     //if any exception happens, the transaction is canceled
     @Transactional(rollbackOn = Exception.class)
-    public void registerUser(UserRegisterDTO userRegisterDTO) throws EntityAlreadyExistsException {
+    public void registerUser(UserRegisterDTO userRegisterDTO) throws EntityAlreadyExistsException{
         try {
-//            if (userRepository.findByUsername(userRegisterDTO.getUsername()).isPresent() ||
-//                    userRepository.findByEmail(userRegisterDTO.getEmail()).isPresent())
-//                throw EntityAlreadyExistsException("User", "User with username: " + userRegisterDTO.getUsername() +
-//                        " and email: " + userRegisterDTO.getEmail() + " already exists.");
-//
-//            User user = mapper.mapToUserEntity
+            if (userRepository.existsByUsername(userRegisterDTO.getUsername())||
+                    userRepository.existsByEmail(userRegisterDTO.getEmail())) {
+                throw new EntityAlreadyExistsException("User", "User with username: " + userRegisterDTO.getUsername() +
+                        " and email: " + userRegisterDTO.getEmail() + " already exists.");
+            }
 
+            userRepository.save(new User());
+            log.info("User with username={} and email={} has registered successfully.", userRegisterDTO.getUsername(), userRegisterDTO.getEmail());
 
-
-        } catch (){
-
+        } catch (EntityAlreadyExistsException e){
+            log.error("You cannot register with username={} and email={}. User already exists.",userRegisterDTO.getUsername(),userRegisterDTO.getEmail(), e);
+            throw e;
         }
     }
 
     @Override
     @Transactional(rollbackOn = Exception.class)
-    public void editUser(UserEditDTO userEditDTO) throws NotExistingEntityException {
+    public void editUser(UserEditDTO userEditDTO) throws NotExistingEntityException, EntityAlreadyExistsException {
+        try {
+            if (!userRepository.existsByUsername(userEditDTO.getUsername())) {
+                throw new NotExistingEntityException("User", "User with username: " + userEditDTO.getUsername()
+                               + " does not exist.");
+            }
+            if (userRepository.existsByUsername(userEditDTO.getUsername()) ||
+                    userRepository.existsByEmail(userEditDTO.getEmail())) {
+                throw new EntityAlreadyExistsException("User", "User with username: " +userEditDTO.getUsername() + " and email: " +userEditDTO.getEmail() + " already exists.");
+            }
 
+//            userRepository.save(userRepository.findByUsername(userEditDTO.getUsername()).);
+            log.info("User with username={} has updated successfully.", userEditDTO.getUsername());
+
+        }catch (NotExistingEntityException e) {
+            log.error("User with username={} does not exist.",userEditDTO.getUsername(), e);
+            throw e;
+        } catch (EntityAlreadyExistsException e) {
+            log.error("User with username={} already exists.",userEditDTO.getUsername(), e);
+            throw e;
+        }
     }
 
     @Override
     @Transactional(rollbackOn = Exception.class)
     public void saveUser(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException {
+        try {
+            if (userRepository.existsByEmail(userInsertDTO.getEmail()) ||
+                    userRepository.existsByUsername(userInsertDTO.getUsername())) {
+                throw new EntityAlreadyExistsException("User", "User with username: " +userInsertDTO.getUsername()
+                        + " and email: " + userInsertDTO.getEmail() + " Already exists.");
+            }
 
+            userRepository.save(new User());
+            log.info("User with username={} and email={} has been saved successfully.", userInsertDTO.getUsername(), userInsertDTO.getEmail());
+        } catch (EntityAlreadyExistsException e) {
+            log.error("User with username={} and email={} already exists.",userInsertDTO.getUsername(),userInsertDTO.getEmail(), e);
+            throw e;
+        }
     }
 }
