d5f8d94ab24f7416527b29b667925b3860594575
elenintoulou <elenintoulou@hotmail.com>
2025-10-27 21:58:16 +0200

    delete RoleRepository because i kept Role as enum and not an entity

--- src/main/java/gr/shareabite/app/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package gr.shareabite.app.repository;
-
-import gr.shareabite.app.enums.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
