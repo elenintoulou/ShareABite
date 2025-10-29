9e7f55cc1315956a15dc1491196bcd764f2e90ac
elenintoulou <elenintoulou@hotmail.com>
2025-10-29 23:44:15 +0200

    Feat: more mapper methods

--- src/main/java/gr/shareabite/app/mapper/Mapper.java
+++ src/main/java/gr/shareabite/app/mapper/Mapper.java
@@ -1,5 +1,6 @@
 package gr.shareabite.app.mapper;
 
+import gr.shareabite.app.dto.UserEditDTO;
 import gr.shareabite.app.dto.UserInsertDTO;
 import gr.shareabite.app.dto.UserReadOnlyDTO;
 import gr.shareabite.app.model.User;
@@ -22,5 +23,20 @@ public class Mapper {
     public UserReadOnlyDTO mapToUserReadOnlyDTO(User user) {
         return new UserReadOnlyDTO(user.getId(), user.getUuid(), user.getUsername(), user.getEmail(),
                 user.getPhoneNumber(),  user.getRole(), user.getRegion(), user.getCreatedAt(), user.getUpdatedAt());
+    }
+
+    public UserEditDTO mapToUserEditDTO(User user) {
+        return new UserEditDTO(user.getUsername(), user.getPassword(), user.getRole(), user.getRegion(), user.getEmail()
+                , user.getPhoneNumber());
+    }
+
+    public void applyEdits(User u, UserEditDTO userEditDTO) {
+        u.setEmail(userEditDTO.getEmail());
+        u.setPhoneNumber(userEditDTO.getPhoneNumber());
+        u.setRegion(userEditDTO.getRegion());
+        u.setRole(userEditDTO.getRole());
+        if (userEditDTO.getPassword() != null && !userEditDTO.getPassword().isBlank()) {
+            u.setPassword(userEditDTO.getPassword()); // TODO: encode όταν βάλεις security
+        }
     }
 }
