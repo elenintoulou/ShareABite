6d28645a27cf81d863f2361da97b4f8026409f94
elenintoulou <elenintoulou@hotmail.com>
2025-10-30 18:24:45 +0200

    Feat: more mapper methods

--- src/main/java/gr/shareabite/app/mapper/Mapper.java
+++ src/main/java/gr/shareabite/app/mapper/Mapper.java
@@ -3,6 +3,7 @@ package gr.shareabite.app.mapper;
 import gr.shareabite.app.dto.UserEditDTO;
 import gr.shareabite.app.dto.UserInsertDTO;
 import gr.shareabite.app.dto.UserReadOnlyDTO;
+import gr.shareabite.app.dto.UserRegisterDTO;
 import gr.shareabite.app.model.User;
 import org.springframework.stereotype.Component;
 
@@ -30,13 +31,33 @@ public class Mapper {
                 , user.getPhoneNumber());
     }
 
-    public void applyEdits(User u, UserEditDTO userEditDTO) {
-        u.setEmail(userEditDTO.getEmail());
-        u.setPhoneNumber(userEditDTO.getPhoneNumber());
-        u.setRegion(userEditDTO.getRegion());
-        u.setRole(userEditDTO.getRole());
+    public void applyEdits(User user, UserEditDTO userEditDTO) {
+        user.setEmail(userEditDTO.getEmail());
+        user.setPhoneNumber(userEditDTO.getPhoneNumber());
+        user.setRegion(userEditDTO.getRegion());
+        user.setRole(userEditDTO.getRole());
         if (userEditDTO.getPassword() != null && !userEditDTO.getPassword().isBlank()) {
-            u.setPassword(userEditDTO.getPassword()); // TODO: encode όταν βάλεις security
+            user.setPassword(userEditDTO.getPassword()); // TODO: encode όταν βάλεις security
         }
+    }
+
+    public UserRegisterDTO mapToUserRegisterDTO(User user) {
+        return new UserRegisterDTO(user.getUsername(), user.getPassword(), user.getEmail(), user.getRegion());
+    }
+
+    public void applyRegistration(User user, UserRegisterDTO userRegisterDTO) {
+        user.setEmail(userRegisterDTO.getEmail());
+        user.setRegion(userRegisterDTO.getRegion());
+        if (userRegisterDTO.getPassword() != null && !userRegisterDTO.getPassword().isBlank()) {
+            user.setPassword(userRegisterDTO.getPassword()); // TODO: encode όταν βάλεις security
+        }
+    }
+    public User mapToEntity(UserRegisterDTO userRegisterDTO) {
+        return User.builder()
+                .username(userRegisterDTO.getUsername())
+                .password(userRegisterDTO.getPassword())
+                .email(userRegisterDTO.getEmail())
+                .region(userRegisterDTO.getRegion())
+                .build();
     }
 }
