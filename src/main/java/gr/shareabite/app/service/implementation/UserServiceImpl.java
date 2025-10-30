1ff7d06d22bb9226f0f178562f77ee19437a9899
elenintoulou <elenintoulou@hotmail.com>
2025-10-30 18:27:44 +0200

    Fix the register method adding mapper

--- src/main/java/gr/shareabite/app/service/implementation/UserServiceImpl.java
+++ src/main/java/gr/shareabite/app/service/implementation/UserServiceImpl.java
@@ -3,8 +3,10 @@ package gr.shareabite.app.service.implementation;
 import gr.shareabite.app.dto.UserEditDTO;
 import gr.shareabite.app.dto.UserInsertDTO;
 import gr.shareabite.app.dto.UserRegisterDTO;
+import gr.shareabite.app.enums.Role;
 import gr.shareabite.app.exception.EntityAlreadyExistsException;
 import gr.shareabite.app.exception.NotExistingEntityException;
+import gr.shareabite.app.mapper.Mapper;
 import gr.shareabite.app.model.User;
 import gr.shareabite.app.repository.UserRepository;
 import gr.shareabite.app.service.interfaces.IUserService;
@@ -22,7 +24,8 @@ public class UserServiceImpl implements IUserService {
 
     //Dependency injection
     private final UserRepository userRepository;
-  //add a mapper
+    private final Mapper mapper;
+
     //i should add a class for authentication
     //private final RoleRepository roleRepository;
 
@@ -37,8 +40,14 @@ public class UserServiceImpl implements IUserService {
                 throw new EntityAlreadyExistsException("User", "User with username: " + userRegisterDTO.getUsername() +
                         " and email: " + userRegisterDTO.getEmail() + " already exists.");
             }
-            userRepository.save(new User());
-            log.info("User with username={} and email={} has registered successfully.", userRegisterDTO.getUsername(), userRegisterDTO.getEmail());
+
+            User user = mapper.mapToEntity(userRegisterDTO);
+            user.setRole(Role.USER);
+
+        // Αποθήκευση στη βάση
+            userRepository.save(user);
+            log.info("User with username={} and email={} has registered successfully.", userRegisterDTO.getUsername(),
+                userRegisterDTO.getEmail());
     }
 
     @Override
@@ -51,7 +60,8 @@ public class UserServiceImpl implements IUserService {
             //check if email doesn't belong to someone else
             var owner = userRepository.findByEmail(userEditDTO.getEmail());
             if (owner.isPresent() && !owner.get().getUsername().equals(userEditDTO.getUsername())) {
-                throw new EntityAlreadyExistsException("User", "User with username: " +userEditDTO.getUsername() + " and email: " +userEditDTO.getEmail() + " already exists.");
+                throw new EntityAlreadyExistsException("User", "User with username: " +userEditDTO.getUsername()
+                        + " and email: " +userEditDTO.getEmail() + " already exists.");
             }
 
 //            userRepository.save(userRepository.findByUsername(userEditDTO.getUsername()).);
