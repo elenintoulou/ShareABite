1585f8211abeb399e06fbb4ee84f2ceb97da0aa7
elenintoulou <elenintoulou@hotmail.com>
2025-11-01 17:30:39 +0200

    Add a dto for the admin to be able to give roles

---
+++ src/main/java/gr/shareabite/app/dto/AdminRoleUpdateDTO.java
@@ -0,0 +1,21 @@
+package gr.shareabite.app.dto;
+
+import gr.shareabite.app.enums.Role;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class AdminRoleUpdateDTO {
+
+    @NotNull
+    private Long id;
+
+    @NotNull(message = "Role cannot be null.")
+    private Role role;
+}
