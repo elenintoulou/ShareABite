613860b63a98231d79427fa3fa92fd84fcb3a5a2
elenintoulou <elenintoulou@hotmail.com>
2025-11-02 12:40:10 +0200

    Update: registerUser controller's logic

---
+++ src/main/java/gr/shareabite/app/controller/UserController.java
@@ -0,0 +1,56 @@
+package gr.shareabite.app.controller;
+
+import gr.shareabite.app.dto.UserRegisterDTO;
+import gr.shareabite.app.exception.EntityAlreadyExistsException;
+import gr.shareabite.app.service.implementation.UserServiceImpl;
+import gr.shareabite.app.service.interfaces.IUserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    //Dependency injection the Interface and not the implementation!!!!
+    private final IUserService iUserService;
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("user", new UserRegisterDTO());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@Valid @ModelAttribute("user") UserRegisterDTO userRegisterDTO,
+                               BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        try {
+            iUserService.registerUser(userRegisterDTO);
+        } catch (EntityAlreadyExistsException e) {
+            // Here you decide *where* to show the error:
+            if (e.getMessage().contains("username")) {
+                bindingResult.rejectValue("username", null, "This username is already taken.");
+            } else if (e.getMessage().contains("email")) {
+                bindingResult.rejectValue("email", null, "This email is already registered.");
+            } else {
+                // fallback → global error
+                bindingResult.reject(null, e.getMessage());
+            }
+
+            return "register";
+        }
+
+        return "redirect:/login";
+    }
+}
