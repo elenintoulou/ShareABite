4f4f274c6984faf6c85456f7ca37fba3292e844b
elenintoulou <elenintoulou@hotmail.com>
2025-11-03 19:13:37 +0200

    Add security details service

---
+++ src/main/java/gr/shareabite/app/authentication/UserDetailsServiceLogIn.java
@@ -0,0 +1,35 @@
+package gr.shareabite.app.authentication;
+
+import gr.shareabite.app.model.User;
+import gr.shareabite.app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceLogIn implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with username: " + username));
+
+        List<SimpleGrantedAuthority> authorities =
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword()) // ΜΕΤΑ το encode στο register ✅
+                .authorities(authorities)
+                .build();
+    }
+}
