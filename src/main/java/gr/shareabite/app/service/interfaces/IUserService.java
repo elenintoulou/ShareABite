6336cc7944df780aab8d4e25a3937131635b13e3
elenintoulou <elenintoulou@hotmail.com>
2025-11-25 17:46:06 +0200

    fix: update parameters in editUser method

--- src/main/java/gr/shareabite/app/service/interfaces/IUserService.java
+++ src/main/java/gr/shareabite/app/service/interfaces/IUserService.java
@@ -1,15 +1,15 @@
 package gr.shareabite.app.service.interfaces;
 
 import gr.shareabite.app.dto.*;
-import gr.shareabite.app.exception.EntityAlreadyExistsException;
-import gr.shareabite.app.exception.NotExistingEntityException;
+import gr.shareabite.app.core.exception.EntityAlreadyExistsException;
+import gr.shareabite.app.core.exception.NotExistingEntityException;
 import org.springframework.data.domain.Page;
 
 public interface IUserService {
 
     void saveUser(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException;
     void registerUser(UserRegisterDTO userRegisterDTO) throws EntityAlreadyExistsException;
-    void editUser(UserEditDTO userEditDTO) throws NotExistingEntityException, EntityAlreadyExistsException;
+    void editUser(String userUuid, UserEditDTO userEditDTO) throws NotExistingEntityException, EntityAlreadyExistsException;
     void changePassword(Long userId, PasswordChangeDTO passwordChangeDTO) throws NotExistingEntityException;
     Page<UserReadOnlyDTO> getPaginatedUsers(int page, int size);
 }
