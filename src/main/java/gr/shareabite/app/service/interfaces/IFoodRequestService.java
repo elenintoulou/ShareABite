b6ef2751bac8200c1fb48e3bd0b50f8010dd137e
elenintoulou <elenintoulou@hotmail.com>
2025-11-26 18:28:33 +0200

    fix: add exceptions

--- src/main/java/gr/shareabite/app/service/interfaces/IFoodRequestService.java
+++ src/main/java/gr/shareabite/app/service/interfaces/IFoodRequestService.java
@@ -1,18 +1,17 @@
 package gr.shareabite.app.service.interfaces;
 
 import gr.shareabite.app.core.enums.Status;
+import gr.shareabite.app.core.exception.NotExistingEntityException;
 import gr.shareabite.app.dto.FoodRequestCreateDTO;
 import gr.shareabite.app.dto.FoodRequestReadOnlyDTO;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 
 public interface IFoodRequestService {
-    void saveFoodRequest(FoodRequestCreateDTO foodRequestCreateDTO);
+    void saveFoodRequest(FoodRequestCreateDTO foodRequestCreateDTO) throws NotExistingEntityException;
     Page<FoodRequestReadOnlyDTO> getPaginatedFoodRequests(int page, int size);
-    FoodRequestReadOnlyDTO getFoodRequestById(Long id);
+    FoodRequestReadOnlyDTO getFoodRequestById(Long id) throws EntityNotFoundException;
     void updateFoodRequest(Long id, FoodRequestCreateDTO foodRequestCreateDTO);
     void deleteFoodRequest(Long id);
     void updateFoodRequestStatus(Long id, Status status);
-
-
-
 }
