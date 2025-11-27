8e02278d35fc6839b8cfeaf7aff88072493065e2
elenintoulou <elenintoulou@hotmail.com>
2025-11-27 19:04:37 +0200

    fix: change the edit user interface

--- src/main/java/gr/shareabite/app/service/interfaces/IUserService.java
+++ src/main/java/gr/shareabite/app/service/interfaces/IUserService.java
@@ -9,7 +9,8 @@ public interface IUserService {
 
     void saveUser(UserInsertDTO userInsertDTO) throws EntityAlreadyExistsException;
     void registerUser(UserRegisterDTO userRegisterDTO) throws EntityAlreadyExistsException;
-    void editUser(String userUuid, UserEditDTO userEditDTO) throws NotExistingEntityException, EntityAlreadyExistsException;
     void changePassword(Long userId, PasswordChangeDTO passwordChangeDTO) throws NotExistingEntityException;
     Page<UserReadOnlyDTO> getPaginatedUsers(int page, int size);
+    UserEditDTO getCurrentUserForEdit();
+    void updateCurrentUser(UserEditDTO userEditDTO) throws EntityAlreadyExistsException;
 }
