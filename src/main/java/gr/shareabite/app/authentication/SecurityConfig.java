7e7beb60da3b4724d0e4b0c06d0fbfe34938aa3a
elenintoulou <elenintoulou@hotmail.com>
2025-12-02 18:36:40 +0200

    fix: add annotation and more url in the filterchain

--- src/main/java/gr/shareabite/app/authentication/SecurityConfig.java
+++ src/main/java/gr/shareabite/app/authentication/SecurityConfig.java
@@ -1,14 +1,15 @@
 package gr.shareabite.app.authentication;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -18,7 +19,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
 
                         //Mipws na tsekarw pali poioi na mpainoun pantou
-                        .requestMatchers("/", "/user/**", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/aboutus", "/user/**", "/css/**", "/js/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
