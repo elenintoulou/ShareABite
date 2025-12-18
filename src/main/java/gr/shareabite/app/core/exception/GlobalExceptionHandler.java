881119714ed97981970eb2e1e81789d5938aeafd
elenintoulou <elenintoulou@hotmail.com>
2025-12-18 17:20:10 +0200

    add: add comment

--- src/main/java/gr/shareabite/app/core/exception/GlobalExceptionHandler.java
+++ src/main/java/gr/shareabite/app/core/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+//Global error handler is a safety net, if any of the exceptions i have below be thrown in a controller and will not be
+//caught, it will be handled here!!!!!!!
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
