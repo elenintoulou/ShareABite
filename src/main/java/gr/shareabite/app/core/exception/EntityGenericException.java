36c59bcc9b3b8f17669226ca8e86fdf0fee2431e
elenintoulou <elenintoulou@hotmail.com>
2026-01-15 00:22:15 +0200

    cleanup: remove unused files

--- src/main/java/gr/shareabite/app/core/exception/EntityGenericException.java
+++ src/main/java/gr/shareabite/app/core/exception/EntityGenericException.java
@@ -1,11 +1,11 @@
-package gr.shareabite.app.core.exception;
-
-public class EntityGenericException extends Exception {
-    private final String code;
-
-    public EntityGenericException(String code, String message) {
-
-        super(message);
-        this.code = code;
-    }
-}
+//package gr.shareabite.app.core.exception;
+//
+//public class EntityGenericException extends Exception {
+//    private final String code;
+//
+//    public EntityGenericException(String code, String message) {
+//
+//        super(message);
+//        this.code = code;
+//    }
+//}
